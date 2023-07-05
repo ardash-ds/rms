1c963f31f0961c30e0b0589b213819d3a3467fcb
sergey.dmitriev <info1981@yandex.ru>
2023-07-05 21:43:52 +0300

    Added CORS

--- rms/rms/settings.py
+++ rms/rms/settings.py
@@ -235,3 +235,27 @@ SPECTACULAR_SETTINGS = {
     'COMPONENT_SPLIT_REQUEST': True,
     # OTHER SETTINGS
 }
+
+# CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default = [])
+CORS_ALLOW_ALL_ORIGINS = DEBUG
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]

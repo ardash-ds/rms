a2249af667de2cedaf61275257f8b04d4cade6fc
sergey.dmitriev <info1981@yandex.ru>
2023-07-06 21:57:47 +0300

    install cors

--- rms/obj_card/urls.py
+++ rms/obj_card/urls.py
@@ -21,18 +21,17 @@ urlpatterns = [
     path('photo/<int:pk>/delete', views.pic_del, name='photo_delete'),
     path('search/', views.SearchObject.as_view(), name='search'),
     path('public_link/<int:pk>', views.ObjPublic.as_view(), name='public_link'),
-    path('storage/', views.StorageList.as_view(), name='storage_list'),
-    path('storage/<int:pk>/', views.StorageDetail.as_view(), name='storage_detail'),
-    path('storage/<int:pk>/delete', views.StorageDelete.as_view(), name='storage_delete'),
-    path('storage/<int:pk>/edit', views.StorageUpdate.as_view(), name='storage_edit'),
-    # path('storage/create', views.StorageCreate.as_view(), name='storage_create'),
-    path('storage/create', views.storage_add, name='storage_create'),
+    path('storage-user/', views.StorageList.as_view(), name='storage_list'),
+    path('storage-user/<int:pk>/', views.StorageDetail.as_view(), name='storage_detail'),
+    path('storage-user/<int:pk>/delete', views.StorageDelete.as_view(), name='storage_delete'),
+    path('storage-user/<int:pk>/edit', views.StorageUpdate.as_view(), name='storage_edit'),
+    path('storage-user/create', views.storage_add, name='storage_create'),
 
     # Адреса для загрузки каталога
-    path('load_cat/', views.load_cat),
-    path('new_load_cat/', views.new_load_cat),
-    path('remove_duplicates/', views.remove_duplicates),
-    path('set_parent/', views.set_parent),
+    # path('load_cat/', views.load_cat),
+    # path('new_load_cat/', views.new_load_cat),
+    # path('remove_duplicates/', views.remove_duplicates),
+    # path('set_parent/', views.set_parent),
 
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns = format_suffix_patterns(urlpatterns)

a102f7eae542dc3110bb6b3a9ab793de75c548ed
Ardash12 <info1981@yandex.ru>
2023-09-13 16:24:50 +0300

    added cookies

--- config/settings.py
+++ config/settings.py
@@ -272,7 +272,7 @@ CORS_ALLOW_HEADERS = [
 ]
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,

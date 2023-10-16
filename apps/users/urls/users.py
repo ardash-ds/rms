d9b23ebab6120cb04d0d80a09e46851427cea584
Ardash12 <info1981@yandex.ru>
2023-10-16 15:55:46 +0300

    added get storage with things

--- apps/users/urls/users.py
+++ apps/users/urls/users.py
@@ -5,10 +5,10 @@ from ..views import refresh_token_cookies, sign_up, sign_in, sign_in_cookies
 
 
 urlpatterns = [
-    path('sign_in_base/', TokenObtainPairView.as_view(), name='sign_in_base'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('sign_up/', sign_up, name='sign_up'),
     path('sign_in/', sign_in, name='sign_in'),
-    path('sign_in_cookies/', sign_in_cookies, name='sign_in_cookies'),
-    path('token/refresh_cookies/', refresh_token_cookies, name='refresh_token_cookies'),
+    # path('sign_in_base/', TokenObtainPairView.as_view(), name='sign_in_base'),
+    # path('sign_in_cookies/', sign_in_cookies, name='sign_in_cookies'),
+    path('sign_up/', sign_up, name='sign_up'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('token/refresh_cookies/', refresh_token_cookies, name='refresh_token_cookies'),
 ]

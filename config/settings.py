16fc95787ca051a2b0fe9a6939d2360a7ee2be99
ardash <info1981@yandex.ru>
2023-12-20 17:35:48 +0300

    added nginx

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,7 @@ DATABASES = {
         'NAME': env.str('DB_NAME'),
         'USER': env.str('DB_USERNAME'),
         'PASSWORD': env.str('DB_PASSWORD'),
-        'HOST': env.str('DB_HOSTNAME'),
+        'HOST': env.str('DB_HOST'),
         'PORT': env.int('DB_PORT'),
     }
 }
